2b8aa14978142614cc30b4e3d5ff9dbce67f3e22
Przemek Kaleta <przemyslawkaleta@gmail.com>
2019-09-14 23:20:50 +0200

    spring security 1 percentage

---
+++ src/main/java/pl/coderslab/charity/controllers/SimpleHelloController.java
@@ -0,0 +1,15 @@
+package pl.coderslab.charity.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleHelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello there";
+    }
+}
